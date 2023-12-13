c1edc1e5fad95bdba922f1e87aac253effac6c47
sbtalk71 <sbtalk@yahoo.com>
2023-12-13 11:27:45 +0530

    updated day3

---
+++ day3/src/streams/StreamDemo2.java
@@ -0,0 +1,20 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamDemo2 {
+
+	public static void main(String[] args) {
+		
+		List<String> namesList=Arrays.asList("Ranga","Kirti","shantanu","Srinivas","shankerlal","Rajeev","Shakuntala");
+		
+		//display all the names with more than 7 chars long
+		//print the length of each name
+		//calculate the sum of the length of all the names using reduce(..)
+		// add Welcome to each name and display
+		//find the name with max length
+		// find the first name in the list
+		//find the last name in the list
+	}
+}
